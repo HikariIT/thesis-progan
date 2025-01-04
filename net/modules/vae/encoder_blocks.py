2f1a604e90f35d269801c5cf9d404fdce4a77da8
HikariIT <alice.it.mail@gmail.com>
2025-01-04 17:42:47 +0100

    Pre-VAE change

--- net/modules/vae/encoder_blocks.py
+++ net/modules/vae/encoder_blocks.py
@@ -4,22 +4,12 @@ import torch.nn.functional as F
 
 class VAEEncoderBlock(nn.Module):
 
-    def __init__(self, in_channels: int, out_channels: int, kernel_size: int):
+    def __init__(self, in_channels: int, out_channels: int, kernel_size: int, stride: int, padding: int):
         super().__init__()
 
-        self.conv = nn.Conv2d(in_channels, out_channels // 2, kernel_size, 2, kernel_size // 2)
-        self.bn_1 = nn.BatchNorm2d(out_channels)
-
-        self.conv_2 = nn.Conv2d(out_channels // 2, out_channels, kernel_size, 1, kernel_size // 2)
-        self.bn_2 = nn.BatchNorm2d(out_channels)
-
-        self.residual_conv = nn.Conv2d(in_channels, out_channels, kernel_size, 2, kernel_size // 2)
-        self.activation = nn.LeakyReLU(0.2)
-
+        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding)
+        self.bn = nn.BatchNorm2d(out_channels)
 
     def forward(self, x):
-        residual = self.residual_conv(x)
-        x = self.activation(self.bn_1(self.conv(x)))
-        x = self.conv_2(x)
-        return self.activation(self.bn_2(x) + residual)
-
+        x = F.leaky_relu(self.bn(self.conv(x)), 0.2)
+        return x
